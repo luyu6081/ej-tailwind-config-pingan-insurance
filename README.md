c088ec332ed789208d381eab19dd03c12737bef5
hz_dfjx_yst_zsj <haitao.zhang@seaboxdata.com>
2020-07-12 12:31:13 +0800

    ej-taillwind

---
+++ README.md
@@ -0,0 +1,24 @@
+# EJ Tailwind Config
+
+## 使用
+
+```js
+/* tailwind.config.js */
+
+const config = require('@ej/tailwind-config')
+
+module.exports = config
+```
+
+## 开发
+
+```sh
+# 安装项目依赖
+yarn install
+
+# 启动预览页面
+yarn serve
+
+# 构建可部署的预览页面
+yarn build
+```
---
+++ src/preview/app.vue
@@ -0,0 +1,20 @@
+<template src="./preview.html"></template>
+
+<script>
+  export default {
+    data () {
+      return {
+        borderRadius: 'default',
+      }
+    },
+
+    computed: {
+      borderRadiusClass () {
+        return this.borderRadius === 'default' ? 'rounded' : 'rounded-' + this.borderRadius
+      },
+    },
+  }
+</script>
+
+<style src="./tailwind.css"></style>
+<style src="./preview.scss" lang="scss"></style>
